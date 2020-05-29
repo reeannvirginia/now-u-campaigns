53d8f802e72bf63643e474d01e17c61acd3cf72a
Reeann <reeannvirginia@gmail.com>
2020-05-28 20:54:08 -0400

    Updates

---
+++ precache-manifest.a60ec43170becf3a002c443a1ff35a5e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923138ec2ff9e3b0a6f385c01734edd4",
+    "url": "/now-u-campaigns/index.html"
+  },
+  {
+    "revision": "6e0d5a0cd32742b28e80",
+    "url": "/now-u-campaigns/static/css/main.7d4b1030.chunk.css"
+  },
+  {
+    "revision": "ceb00760eb98c5917406",
+    "url": "/now-u-campaigns/static/js/2.65fee542.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/now-u-campaigns/static/js/2.65fee542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e0d5a0cd32742b28e80",
+    "url": "/now-u-campaigns/static/js/main.7dabafe8.chunk.js"
+  },
+  {
+    "revision": "157c314b57cb5f135266",
+    "url": "/now-u-campaigns/static/js/runtime-main.23d60944.js"
+  },
+  {
+    "revision": "9377dd96daab52a0dfd5855f3536cea3",
+    "url": "/now-u-campaigns/static/media/app-download.9377dd96.svg"
+  },
+  {
+    "revision": "5130b02068eda830719df051f5a218ee",
+    "url": "/now-u-campaigns/static/media/apple-store.5130b020.svg"
+  },
+  {
+    "revision": "9932622c11a69df1d938f1c0d255fac8",
+    "url": "/now-u-campaigns/static/media/google-store.9932622c.svg"
+  },
+  {
+    "revision": "23fe7730c04158d02dd17889b2067b59",
+    "url": "/now-u-campaigns/static/media/now-u-logo-orange.23fe7730.svg"
+  },
+  {
+    "revision": "02c2e18c7df0e903acc79f4d84e00796",
+    "url": "/now-u-campaigns/static/media/our_story_img.02c2e18c.png"
+  },
+  {
+    "revision": "c0d9ab35ae0800f61c6fcfc97486415b",
+    "url": "/now-u-campaigns/static/media/road-sign.c0d9ab35.svg"
+  },
+  {
+    "revision": "465387fd7034cea8081cb201dcc74b58",
+    "url": "/now-u-campaigns/static/media/the_app.465387fd.png"
+  }
+]);
